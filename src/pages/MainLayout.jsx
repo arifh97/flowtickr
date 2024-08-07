35f47b00526d23605eeb88889736844841528a72
arif.h97 <arifline16@gmail.com>
2024-08-07 22:21:41 +0600

    pull

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import LeftSidebar from '../components/dashbaord/LeftSidebar'
 import TopBar from '../components/dashbaord/TopBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 export default function MainLayout() {
   const [leftSide, setLeftSide] = useState(false);
   const showLeftSide = () => {
     setLeftSide(!leftSide)
-    console.log(leftSide);
   }
+  const pathname = useLocation();
+  useEffect(() => {
+    window.scrollTo(0,0)
+  },[pathname])
   return (
     <div className='dashboard d-flex flex-wrap'>
       <div className={`dashboard-left vh-100 ${leftSide&&'showSidebar'}`}>
