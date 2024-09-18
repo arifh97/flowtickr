467c49edcb42e5eddb8f1fe9ed592dbdb1feabd0
mehedi3365 <mehedihassanemon369@gmail.com>
2024-09-18 22:56:15 +0600

    login page update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,10 +5,23 @@ export default function Contact() {
     <div className='contact'>
       <div className="contact-wrap bg-white">
         <h3 className='text-center'>Contact Us</h3>
-        <div className="form-box">
-          <label htmlFor="#" className="form-label">Name</label>
-          <input type="text" placeholder='Type here' className="form-control" />
-        </div>
+        <form action="">
+          <div className="form-box">
+            <label htmlFor="#" className="form-label">Name</label>
+            <input type="text" placeholder='Type here' className="form-control" required />
+          </div>
+          <div className="form-box">
+            <label htmlFor="Email" className="form-label">Name</label>
+            <input type="email" placeholder='Email' className="form-control" required />
+          </div>
+          <div className="form-box">
+            <label htmlFor="#" className="form-label">Your message</label>
+            <textarea name="" className='form-control' placeholder='Type here' id=""></textarea>
+          </div>
+          <div className="w-100 text-center">
+            <button type='submit' className='btn text-center bg-primary'>Contact Us</button>
+          </div>
+        </form>
       </div>
     </div>
   )
