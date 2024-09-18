84846e3956463e16d96c1e6e9ea231566c9d03e7
Hassan Ali <hassan4np@gmail.com>
2024-09-18 21:22:01 +0600

    complate faq

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -3,40 +3,37 @@ import Accordion from 'react-bootstrap/Accordion';
 export default function Help() {
     const faqs = [
         {
-            title: 'Amet minim mollit non deserunt ullamco est sit .',
+            title: 'Amet minim mollit non deserunt ullamco est sit . .',
             des: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
         },
         {
-            title: 'Amet minim mollit non deserunt ullamco est sit .',
+            title: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor..',
             des: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
         },
         {
-            title: 'Amet minim mollit non deserunt ullamco est sit .',
+            title: 'Amet minim mollit non deserunt..',
             des: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
         },
         {
-            title: 'Amet minim mollit non deserunt ullamco est sit .',
+            title: 'Amet minim mollit non deserunt ullamco est sit..',
             des: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
         },
     ]
     return (
-        <div className='help'>
-            <Accordion defaultActiveKey={'0'} alwaysOpen>
-                {faqs.map((item, index) => (
-                    <Accordion.Item eventKey={`${index}`} key={index}>
-                        <Accordion.Header>Accordion Item #1</Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                ))}
-            </Accordion>
+        <div className='help d-flex justify-content-center'>
+            <div className='help-area'>
+                <h2 className='fw-bold text-center mb-3 mb-lg-4' >Frequently Asked Questions</h2>
+                <Accordion defaultActiveKey={'0'} alwaysOpen>
+                    {faqs.map((item, index) => (
+                        <Accordion.Item eventKey={`${index}`} key={index}>
+                            <Accordion.Header>{item.title}</Accordion.Header>
+                            <Accordion.Body>
+                                {item.des}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    ))}
+                </Accordion>
+            </div>
         </div>
     )
 }
