8d7ef55c4680ae0152b8b7d2e0119f49747400a1
arif.h97 <arifline16@gmail.com>
2024-10-03 10:53:33 +0600

    drag drop

--- src/pages/CreateStrategies.jsx
+++ src/pages/CreateStrategies.jsx
@@ -15,6 +15,49 @@ export default function CreateStrategies() {
     setLeftBar(true)
     setTopBar(false)
   }
+
+  const draggableData = [
+    {
+      title: 'Decision',
+      data: [
+        `<span>Symbols</span> Price is <span>Above</span> <span>$100</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+      ]
+    },
+    {
+      title: 'Decision two',
+      data: [
+        `<span>Symbols</span> Price is <span>Above</span> <span>$100</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+        `<span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span>`,
+      ]
+    },
+  ]
+
+  const [droppedItems, setDroppedItems] = useState([]);
+  const handleDragStart = (event, item) => {
+    event.dataTransfer.setData("application/json", JSON.stringify(item));
+  };
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const droppedItem = JSON.parse(
+      event.dataTransfer.getData("application/json")
+    );
+    setDroppedItems([...droppedItems, droppedItem]);
+  };
+
   return (
     <div className='dashboard-wrap p-0'>
       <div className="createStrategies">
@@ -32,8 +75,8 @@ export default function CreateStrategies() {
           </div>
           <div className="header-right d-flex gap-2 gap-xl-3 align-items-center">
             <div className="arrow-btn p-1 d-flex gap-1">
-              <button className={leftBar?'active':''} onClick={() => handleChangeLeftBar()}><BsArrowRight /></button>
-              <button className={topBar?'active':''} onClick={()=> handleChangeTopBar()}><BsArrowDown /></button>
+              <button className={leftBar ? 'active' : ''} onClick={() => handleChangeLeftBar()}><BsArrowRight /></button>
+              <button className={topBar ? 'active' : ''} onClick={() => handleChangeTopBar()}><BsArrowDown /></button>
             </div>
             <div className="header-right-btn d-flex gap-2 gap-xl-3">
               <button className="btn text-white padding">Close</button>
@@ -49,96 +92,39 @@ export default function CreateStrategies() {
           </div>
         </div>
         <div className="createStrategies-wrap d-flex">
-          <div className="createStrategies-leftBar">
+          <div className="createStrategies-leftBar pb-3">
             <p className='action d-flex align-items-center justify-content-between'>Actions Panel <button className="action-btn p-0 border-0 d-flex align-items-center justify-content-center"><LiaAngleLeftSolid /></button></p>
             <div className="action-panel">
-              <div className="single ">
-                <div className="it d-flex justify-content-between align-items-center">
-                  Decision <LiaAngleDownSolid />
-                </div>
-                <div className="dashboard-topBar-search bg-white rounded-3 position-relative z-1 d-none d-md-block">
-                  <div className="icon d-flex align-items-center justify-content-center position-absolute top-50 translate-middle-y ms-3">
-                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M15.75 15.7502L12.4928 12.4929M12.4928 12.4929C13.0499 11.9357 13.4919 11.2743 13.7934 10.5463C14.095 9.81834 14.2502 9.03811 14.2502 8.25015C14.2502 7.4622 14.095 6.68197 13.7934 5.95399C13.4919 5.22602 13.0499 4.56457 12.4928 4.0074C11.9356 3.45024 11.2741 3.00827 10.5462 2.70673C9.81819 2.4052 9.03795 2.25 8.25 2.25C7.46205 2.25 6.68181 2.4052 5.95384 2.70673C5.22587 3.00827 4.56442 3.45024 4.00725 4.0074C2.882 5.13265 2.24985 6.65881 2.24985 8.25015C2.24985 9.84149 2.882 11.3677 4.00725 12.4929C5.1325 13.6182 6.65866 14.2503 8.25 14.2503C9.84134 14.2503 11.3675 13.6182 12.4928 12.4929Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
-                      <path d="M15.75 15.7502L12.4928 12.4929M12.4928 12.4929C13.0499 11.9357 13.4919 11.2743 13.7934 10.5463C14.095 9.81834 14.2502 9.03811 14.2502 8.25015C14.2502 7.4622 14.095 6.68197 13.7934 5.95399C13.4919 5.22602 13.0499 4.56457 12.4928 4.0074C11.9356 3.45024 11.2741 3.00827 10.5462 2.70673C9.81819 2.4052 9.03795 2.25 8.25 2.25C7.46205 2.25 6.68181 2.4052 5.95384 2.70673C5.22587 3.00827 4.56442 3.45024 4.00725 4.0074C2.882 5.13265 2.24985 6.65881 2.24985 8.25015C2.24985 9.84149 2.882 11.3677 4.00725 12.4929C5.1325 13.6182 6.65866 14.2503 8.25 14.2503C9.84134 14.2503 11.3675 13.6182 12.4928 12.4929Z" stroke="currentColor" strokeOpacity="0.2" strokeLinecap="round" strokeLinejoin="round"></path>
-                    </svg>
+              {draggableData.map((item, index) => (
+                <div className="single" key={index}>
+                  <div className="it d-flex justify-content-between align-items-center">
+                    {item.title} <LiaAngleDownSolid />
                   </div>
-                  <input type="search" placeholder="Search" className="form-control border-0 bg-transparent" />
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Above</span> <span>$100</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-              </div>
-              <div className="single ">
-                <div className="it d-flex justify-content-between align-items-center">
-                  Decision <LiaAngleDownSolid />
-                </div>
-                <div className="dashboard-topBar-search bg-white rounded-3 position-relative z-1 d-none d-md-block">
-                  <div className="icon d-flex align-items-center justify-content-center position-absolute top-50 translate-middle-y ms-3">
-                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M15.75 15.7502L12.4928 12.4929M12.4928 12.4929C13.0499 11.9357 13.4919 11.2743 13.7934 10.5463C14.095 9.81834 14.2502 9.03811 14.2502 8.25015C14.2502 7.4622 14.095 6.68197 13.7934 5.95399C13.4919 5.22602 13.0499 4.56457 12.4928 4.0074C11.9356 3.45024 11.2741 3.00827 10.5462 2.70673C9.81819 2.4052 9.03795 2.25 8.25 2.25C7.46205 2.25 6.68181 2.4052 5.95384 2.70673C5.22587 3.00827 4.56442 3.45024 4.00725 4.0074C2.882 5.13265 2.24985 6.65881 2.24985 8.25015C2.24985 9.84149 2.882 11.3677 4.00725 12.4929C5.1325 13.6182 6.65866 14.2503 8.25 14.2503C9.84134 14.2503 11.3675 13.6182 12.4928 12.4929Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
-                      <path d="M15.75 15.7502L12.4928 12.4929M12.4928 12.4929C13.0499 11.9357 13.4919 11.2743 13.7934 10.5463C14.095 9.81834 14.2502 9.03811 14.2502 8.25015C14.2502 7.4622 14.095 6.68197 13.7934 5.95399C13.4919 5.22602 13.0499 4.56457 12.4928 4.0074C11.9356 3.45024 11.2741 3.00827 10.5462 2.70673C9.81819 2.4052 9.03795 2.25 8.25 2.25C7.46205 2.25 6.68181 2.4052 5.95384 2.70673C5.22587 3.00827 4.56442 3.45024 4.00725 4.0074C2.882 5.13265 2.24985 6.65881 2.24985 8.25015C2.24985 9.84149 2.882 11.3677 4.00725 12.4929C5.1325 13.6182 6.65866 14.2503 8.25 14.2503C9.84134 14.2503 11.3675 13.6182 12.4928 12.4929Z" stroke="currentColor" strokeOpacity="0.2" strokeLinecap="round" strokeLinejoin="round"></path>
-                    </svg>
+                  <div className="dashboard-topBar-search bg-white rounded-3 position-relative z-1 d-none d-md-block">
+                    <div className="icon d-flex align-items-center justify-content-center position-absolute top-50 translate-middle-y ms-3">
+                      <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.75 15.7502L12.4928 12.4929M12.4928 12.4929C13.0499 11.9357 13.4919 11.2743 13.7934 10.5463C14.095 9.81834 14.2502 9.03811 14.2502 8.25015C14.2502 7.4622 14.095 6.68197 13.7934 5.95399C13.4919 5.22602 13.0499 4.56457 12.4928 4.0074C11.9356 3.45024 11.2741 3.00827 10.5462 2.70673C9.81819 2.4052 9.03795 2.25 8.25 2.25C7.46205 2.25 6.68181 2.4052 5.95384 2.70673C5.22587 3.00827 4.56442 3.45024 4.00725 4.0074C2.882 5.13265 2.24985 6.65881 2.24985 8.25015C2.24985 9.84149 2.882 11.3677 4.00725 12.4929C5.1325 13.6182 6.65866 14.2503 8.25 14.2503C9.84134 14.2503 11.3675 13.6182 12.4928 12.4929Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <path d="M15.75 15.7502L12.4928 12.4929M12.4928 12.4929C13.0499 11.9357 13.4919 11.2743 13.7934 10.5463C14.095 9.81834 14.2502 9.03811 14.2502 8.25015C14.2502 7.4622 14.095 6.68197 13.7934 5.95399C13.4919 5.22602 13.0499 4.56457 12.4928 4.0074C11.9356 3.45024 11.2741 3.00827 10.5462 2.70673C9.81819 2.4052 9.03795 2.25 8.25 2.25C7.46205 2.25 6.68181 2.4052 5.95384 2.70673C5.22587 3.00827 4.56442 3.45024 4.00725 4.0074C2.882 5.13265 2.24985 6.65881 2.24985 8.25015C2.24985 9.84149 2.882 11.3677 4.00725 12.4929C5.1325 13.6182 6.65866 14.2503 8.25 14.2503C9.84134 14.2503 11.3675 13.6182 12.4928 12.4929Z" stroke="currentColor" strokeOpacity="0.2" strokeLinecap="round" strokeLinejoin="round"></path>
+                      </svg>
+                    </div>
+                    <input type="search" placeholder="Search" className="form-control border-0 bg-transparent" />
                   </div>
-                  <input type="search" placeholder="Search" className="form-control border-0 bg-transparent" />
+                  {item.data.map((item, index) => (
+                    <div
+                      className="action-item d-flex align-items-center justify-content-between"
+                      key={index}
+                      draggable
+                      onDragStart={(e) => handleDragStart(e, item)}
+                    >
+                      <p dangerouslySetInnerHTML={{ __html: item }}></p>
+                      {/* <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div> */}
+                    </div>
+                  ))}
                 </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Above</span> <span>$100</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-                <div className="action-item d-flex align-items-center justify-content-between">
-                  <p><span>Symbols</span> Price is <span>Increased %</span> Since <span>Close 1 day Ago</span></p>
-                  <div className="plus bg-primary d-flex align-items-center justify-content-center"><HiOutlinePlusSm /></div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
-          <div className="createStrategies-content d-flex flex-column">
+          <div className="createStrategies-content d-flex flex-column pb-3">
             {topBar &&
               <div className="content-header d-flex mx-auto justify-content-between">
                 <div className="content-header-left d-flex align-items-center gap-2">
@@ -163,13 +149,28 @@ export default function CreateStrategies() {
                 </div>
               </div>
             }
-            <div className="drop mx-auto text-center d-flex align-items-center justify-content-center flex-column">
+            <div
+              className="drop mx-auto text-center d-flex align-items-center justify-content-center flex-column"
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
+            >
               <button className="btn btn1 d-inline-block bg-primary">Start</button>
               <img className='style1' src={lineArrow} alt="" />
-              <div className="drop-btn btn">SPY Price is increased 45%  since close 5 days ago</div>
-              <img className='style1' src={lineArrow} alt="" />
-              <div className="drop-btn btn">INFY if 50 EMA&#62;100 EMA</div>
-              <img className='style1' src={lineArrow} alt="" />
+              {droppedItems.length > 0 ? (
+                <>
+                  {droppedItems.map((item) => (
+                    <div key={item.id}>
+                      <div className="drop-btn btn d-block" dangerouslySetInnerHTML={{ __html: item }}></div>
+                      <img className='style1' src={lineArrow} alt="" />
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <>
+                  <div className="drop-btn btn d-block text-primary">Drop Here</div>
+                  <img className='style1' src={lineArrow} alt="" />
+                </>
+              )}
               <button className="drop-btn btn text-primary gap-2 d-flex align-items-center">
                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M16.5 11.5V14.8333C16.5 15.2754 16.3244 15.6993 16.0118 16.0118C15.6993 16.3244 15.2754 16.5 14.8333 16.5H3.16667C2.72464 16.5 2.30072 16.3244 1.98816 16.0118C1.67559 15.6993 1.5 15.2754 1.5 14.8333V11.5M4.83333 7.33333L9 11.5M9 11.5L13.1667 7.33333M9 11.5V1.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
